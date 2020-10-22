cb3d480e784154ed8cdafdfeec7b98bc21eb4af1
Tom Himanen <tom.himanen@gmail.com>
2020-10-22 12:15:06 +0300

    Toteuttaa taskien lisäämisen ja poistamisen

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import React, { useState } from 'react';
 
 function App() {
 
-  let taskit = ["jutska 1", "jutska 2", "jutska 3", "jutska 4"]
+  const [taskit, paivitaTaskit] = useState([])
 
-  const [uusiTaski, paivitaUusiTaski] = useState("wertwert")
+  const [uusiTaski, paivitaUusiTaski] = useState("")
 
   function lisaaTaski() {
-    alert("Morjesta, täällä nappi moi vaan!")
+    let taskitKopio = [...taskit] // spread-operaattori on ...
+    //taskitKopio.push(uusiTaski)
+    taskitKopio.push({"id": Date.now(), "teksti": uusiTaski })
+    paivitaTaskit(taskitKopio)
+    console.log(taskitKopio)
+    paivitaUusiTaski("")
+  }
+
+  function poistaTaski(taskinId) {
+    let taskitKopio = [...taskit]
+    let karsittuLista = taskitKopio.filter((taski) => taski.id !== taskinId)
+    paivitaTaskit(karsittuLista)
   }
 
   return (
     <div className="App">
       <p>Varmistusheippa</p>
-      <input type="text" value={uusiTaski} />
+      <input type="text" value={uusiTaski} onChange={(e) => paivitaUusiTaski(e.target.value)} />
       <input type="button" value="Lisää taski" onClick={() => lisaaTaski() } />
       <ul>
-        { taskit.map( yksittainenTaski => <li>{yksittainenTaski}</li> )}
+        { taskit.map( yksittainenTaski => 
+          <li>{yksittainenTaski.teksti} <button onClick={() => poistaTaski(yksittainenTaski.id)}>x</button> </li> )
+        }
       </ul>
       {/*
-        lista tehtävistä
         tehtävien poistaminen
       */}
     </div>
